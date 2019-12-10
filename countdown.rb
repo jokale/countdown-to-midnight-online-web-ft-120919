993aa486f5996be44070d70ccf240d3bf60c6a2d
Joanna E K <kalemajoanna@gmail.com>
2019-12-10 22:45:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,9 +9,9 @@ return "HAPPY NEW YEAR!"
 end 
 
 def countdown_with_sleep(number)
-  while runtime > 0 
-  puts "#{runtime} SECOND(S)!"
-  runtime -= 1 
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1 
   sleep(1)
  end 
 end 
